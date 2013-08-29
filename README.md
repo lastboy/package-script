6da67ed9c12a199d145a46c4b982ff840401ed80
arik <lewin.arik@gmail.com>
2013-08-29 14:56:51 +0300

    initial project

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 package-script
 ==============
+
+Add a nodejs call to your package.json script section for spawning an external commands.
+ e.g. install bower or grunt that are global packages as part of your module.
+
+
+First add script section to your package.json file
+
+package.json
+===
+
+Add a script section to your package.json file.
+You can set the script to run in any phase according to the package.json docs
+
+     "scripts": {
+        "install": "node installer.js"
+      }
+
+Then, in your javascript file:
+
+    require('package-script').spawn([
+          {
+              command: "npm",
+              args: ["install", "-g", "grunt-cli"]
+          },
+          {
+              command: "npm",
+              args: ["install", "-g", "bower"]
+          }
+      ]);
---
+++ pkgscript.js
@@ -0,0 +1,123 @@
+"use strict";
+
+var os = require('os'),
+    fs = require("fs"),
+    typedas = require('typedas'),
+    spawn,
+    options = {
+        cachePassword: true,
+        prompt: 'Additional global npm(s) about to be installed, set your root password: ',
+        spawnOptions: { /* other options for spawn */ }
+    },
+    process,
+    commands = [],
+    next= 0, size = commands.length;
+
+function isLinux() {
+    return (os.platform() == "linux");
+}
+
+function init() {
+    var log = "Package Script installer Initialized ....";
+    logall(log);
+
+    if (isLinux()) {
+        spawn = require('sudo');
+    } else {
+        spawn =  require('child_process').spawn;
+    }
+
+}
+
+function log2file(data, option) {
+    try {
+        fs.appendFileSync("pkgscript.log", (data + "\n"), "utf8");
+    } catch (e) {
+        log2file("Package Script, ERROR:", e);
+    }
+}
+
+function logall(msg) {
+    if (msg) {
+        console.log(msg);
+        log2file(msg);
+    }
+}
+
+
+function install(items) {
+
+    var item = items[next],
+        command = item.command,
+        args = item.args,
+        print;
+
+    if (isLinux()) {
+        args.unshift(command);
+        print = args.join(" ");
+        console.log("Running Installer command: " + print);
+        process = spawn(args, options);
+
+    } else {
+        args.unshift(command);
+        args.unshift("/c");
+        command = "cmd";
+        print = [command, args.join(" ")].join(" ");
+        console.log("Running Installer command: " + print);
+        process = spawn(command, args);
+
+    }
+
+
+    process.stdout.on('data', function (data) {
+        var log = 'CAT Installer: ' + data;
+        logall(log);
+    });
+
+    process.stderr.on('data', function (data) {
+        log2file('Package Script : ' + data);
+    });
+
+    process.on('close', function (code) {
+        log2file('Package Script Complete ' + code);
+        next++;
+        if (next < size) {
+            install(items);
+        }
+    });
+}
+
+// Initialization
+(function() {
+    init();
+})();
+
+/**
+ * Executing multiple calls synchronously according to a given configuration
+ *
+ * @type {module.exports}
+ */
+module.exports = function() {
+
+    return {
+
+        /**
+         * spawn additional command according to the config
+         *
+         * @param config The configuration for the install
+         * e.g. [{
+         *          command: 'npm',
+         *          args: ["--version"]
+         *      }]
+         */
+        spawn: function(config) {
+            if (config && typedas.isArray(config)) {
+                install(commands);
+
+            } else {
+                logall("No valid configuration for 'install' function, see the docs for more information ");
+            }
+        }
+    };
+
+}();
